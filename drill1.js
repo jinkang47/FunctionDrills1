d8f3e7907e75ebe05b5fda27f03ff8d6f60c6923
Michael Anokye <michaelanokyej@yahoo.com>
2019-08-13 21:17:58 -0400

    added param

--- drill1.js
+++ drill1.js
@@ -1,8 +1,16 @@
-function createGreeting {
-    return 'Hi, my name is Jinsoo and I\'m 39 years old';
+let name = "Jin";
+let age = 39;
 
+function getYearOfBirth(age){
+    return 2019 - age;
+}
+
+function createGreeting(name, age) {
+    const yob = getYearOfBirth(age);
+    return `I was born in ${yob}`;
 }
 
 const greeting1 = createGreeting();
 console.log(greeting1);
+
 
